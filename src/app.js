dfa400c13fe629610efaa87829b9e87fafa8d0ba
MamaOsaBit <65994187+MamaOsaBit@users.noreply.github.com>
2025-09-11 18:43:18 -0300

    .

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const routes = require('./routes');
 const registerRouter = require('./routes/registerRoutes');
 
@@ -7,8 +8,24 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', routes);
-
 app.use('/', registerRouter);
+
+// Rutas para vistas HTML
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'register.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
+
+app.get('/paciente/solicitar-hora', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'solicitarHora.html'));
+});
+
+app.get('/medico/panel', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'medicoPanel.html'));
+});
 
 app.get('/', (req, res) => {
   res.send('Hola mundo');
